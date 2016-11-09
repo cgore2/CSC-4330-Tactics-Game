f504875876d8bf923d5f189d884a4a7888c7e223
Daniel LaBorde <dlabo14@lsu.edu>
2016-11-09 06:16:17 -0600

    updated

--- Unit.cs
+++ Unit.cs
@@ -1,34 +1,50 @@
 using System;
 using UnityEngine;
+using System.Collections;
 
 namespace UnityStandardAssets._2D
 {
+
 	public class Unit : MonoBehaviour
 	{
-		private int hitPoints = 10;
-		private int attackPoints = 5;
-		//private Square moveRange = 3; 
-		//private Square attackRange = 1;
-		private Square position; 		     //the units current position
-		private int[] status = new int[2]    //array to store HP and AP
-		
-		//moves the unit from its current position(square) to a new square
-		public Square MoveUnit(Square square )
-		{
-			position = square
-			return position;
-		}
+	    private int hitPoints;
+	    private int attackPoints;
+	    private int[] status = new int[2]    //array to store HP and AP
+	    //public Square moveRange; 
+	    //public Square attackRange;
+	    public Square position;             //stores the position of the current unit
 
-		public Attack()
-		{
-			//implement
-		}
 
-		//returns HP and AP in an array
-		private int[] checkStatus()
-		{
-			status[0] = hitPoints;
-			status[1] = attackPoints;
-			return status;
-		}
+	    protected void Start()
+	    {
+		hitPoints = 10;
+		attackPoints = 5;
+	    }
+
+	    // Update is called once per frame
+	    protected void Update()
+	    {
+		hitPoints = hitPoints;
+		attackPoints = attackPoints;
+	    }
+
+	    //moves the unit from its current position(square) to a new square
+	    public void MoveUnit(int x, int y)
+	    {
+	       position = square[x][y];
+	    }
+
+	    public void Attack()
+	    {
+		//implement
+	    }
+
+	    //returns HP and AP in an array
+	    private int[] checkStatus()
+	    {
+		status[0] = hitPoints;
+		status[1] = attackPoints;
+		return status;
+	    }
 	}
+}
