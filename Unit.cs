c44571c3e48faa3bf50dbabbd9f240a80cb89f37
Daniel LaBorde <dlabo14@lsu.edu>
2016-11-09 07:13:42 -0600

    fixed position

--- Unit.cs
+++ Unit.cs
@@ -9,16 +9,21 @@ namespace UnityStandardAssets._2D
 	{
 	    private int hitPoints;
 	    private int attackPoints;
-	    private int[] status = new int[2]    //array to store HP and AP
+	    private int[] status = new int[3]    //array to store HP, AP, and position
 	    //public Square moveRange; 
 	    //public Square attackRange;
-	    public Square position;             //stores the position of the current unit
+	    Unit unit = new Unit();
+	    public Square position;          		//need int[,] square = new int[3,3]{
+	    						//{{0,0}{0,1}{0,2}},
+							//{{1,0}{1,1}{1,2}},
+							//{{2,0}{2,1}{2,2}}; also getSquare() method
 
 
 	    protected void Start()
 	    {
 		hitPoints = 10;
 		attackPoints = 5;
+		position = square[0,0];
 	    }
 
 	    // Update is called once per frame
@@ -26,12 +31,13 @@ namespace UnityStandardAssets._2D
 	    {
 		hitPoints = hitPoints;
 		attackPoints = attackPoints;
+		position = unit.getSquare();
 	    }
 
 	    //moves the unit from its current position(square) to a new square
 	    public void MoveUnit(int x, int y)
 	    {
-	       position = square[x][y];
+	       position = square[x,y];
 	    }
 
 	    public void Attack()
@@ -44,6 +50,7 @@ namespace UnityStandardAssets._2D
 	    {
 		status[0] = hitPoints;
 		status[1] = attackPoints;
+		status[3] = position;
 		return status;
 	    }
 	}
