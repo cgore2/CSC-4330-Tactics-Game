6e08e0fa3d6d67d0bd886581485beac5cb8499a5
Daniel LaBorde <dlabo14@lsu.edu>
2016-11-26 07:22:12 -0600

    added the initial grid

--- Board.java
+++ Board.java
@@ -16,9 +16,44 @@ public class Board extends JPanel implements ActionListener{
 	
 	public Board() {
 		
+		initializeGrid();
 		startGame();
 		//addKeyListener(..)
 		
+	}
+	
+	private void initializeGrid(){
+
+		grid[0][0] = new Square(0,0);
+		grid[0][1] = new Square(0,1);
+		grid[0][2] = new Square(0,2);
+		grid[0][3] = new Square(0,3);
+		grid[0][4] = new Square(0,4);
+   
+		grid[1][0] = new Square(1,0);
+		grid[1][1] = new Square(1,1);
+		grid[1][2] = new Square(1,2);
+		grid[1][3] = new Square(1,3);
+		grid[1][4] = new Square(1,4); 
+
+		grid[2][0] = new Square(2,0);
+		grid[2][1] = new Square(2,1);
+		grid[2][2] = new Square(2,2);
+		grid[2][3] = new Square(2,3);
+		grid[2][4] = new Square(2,4);
+
+		grid[3][0] = new Square(3,0);
+		grid[3][1] = new Square(3,1);
+		grid[3][2] = new Square(3,2);
+		grid[3][3] = new Square(3,3);
+		grid[3][4] = new Square(3,4);
+
+		grid[4][0] = new Square(4,0);
+		grid[4][1] = new Square(4,1);
+		grid[4][2] = new Square(4,2);
+		grid[4][3] = new Square(4,3);
+		grid[4][4] = new Square(4,4);
+
 	}
 	
 	private void startGame(){
