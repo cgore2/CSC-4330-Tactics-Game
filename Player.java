c2e7d079d8707fe16ef8a1221652e728fc04ca25
Daniel LaBorde <dlabo14@lsu.edu>
2016-11-26 17:55:51 -0600

    minor update

--- Player.java
+++ Player.java
@@ -13,14 +13,14 @@ public class Player {
 	
 	}
 	
-	//ADDS the selected unit too the Players array of units
+	//ADDS the selected unit too the Players array of units.. MAX UNITS 3, unitNum can be 1 2 or 3
 	public void AddUnit(String s, int unitNum){
 		if(s == "blue"){
-			unit[unitNum] = new Dolphin(0, 0, 10, 3, 1); // row0 col0 HP10 AP3 AR1
+			unit[unitNum - 1] = new Dolphin(0, 0, 10, 3, 1); // row0 col0 HP10 AP3 AR1
 		}else if(s == "red"){
-			unit[unitNum] = new RedDolphin(0, 0, 10, 3, 1);
+			unit[unitNum - 1] = new RedDolphin(0, 0, 10, 3, 1);
 		}else{
-			//if more units are added unit[0] = new GreenDolphin(0, 0, 10, 3, 1);
+			//if more units are added unit[unitNum - 1] = new GreenDolphin(0, 0, 10, 3, 1);
 		}
 		
 	}
