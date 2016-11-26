d6529c65481694b769689428c7e62daa0841116b
Daniel LaBorde <dlabo14@lsu.edu>
2016-11-26 17:49:22 -0600

    added AddUnit() method

--- Player.java
+++ Player.java
@@ -13,16 +13,24 @@ public class Player extends Board {
 	
 	}
 	
+	//ADDS the selected unit too the Players array of units
+	public void AddUnit(String s, int unitNum){
+		if(s == "blue"){
+			unit[unitNum] = new Dolphin(0, 0, 10, 3, 1); // row0 col0 HP10 AP3 AR1
+		}else if(s == "red"){
+			unit[unitNum] = new RedDolphin(0, 0, 10, 3, 1);
+		}else{
+			//if more units are added unit[0] = new GreenDolphin(0, 0, 10, 3, 1);
+		}
+		
+	}
+	 
 	public void options(){
 		
 	}
 	
 	public int CheckScore(){
 		return score;
-	}
-	
-	public void ControlUnit(){
-		
 	}
 	 
 }
