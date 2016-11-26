f1f99fb6873d8c0cfb8e039000dafe9e6a6afae0
Daniel LaBorde <dlabo14@lsu.edu>
2016-11-26 06:12:22 -0600

    initial code

---
+++ Player.java
@@ -0,0 +1,28 @@
+package dolphingame;
+
+
+public class Player extends Board {
+	
+	public Unit[] unit = new Unit[5];
+	public int turnNumber;
+	public int score;
+	//not sure about playerType
+
+	
+	public Player(){
+	
+	}
+	
+	public void options(){
+		
+	}
+	
+	public int CheckScore(){
+		return score;
+	}
+	
+	public void ControlUnit(){
+		
+	}
+	 
+}
