4fbe56c9a03f85604234289724fde7d1662b9fa0
Daniel LaBorde <dlabo14@lsu.edu>
2016-11-29 13:46:32 -0600

    added documentation

--- Player.java
+++ Player.java
@@ -3,7 +3,6 @@ package data;
 import static helpers.Painter.*;
 
 public class Player {
-	public int currentPos;
 	public boolean isAlive;
 	public int posX, posY;
 	public int startX, startY;
@@ -11,11 +10,14 @@ public class Player {
 	Square s;
 
 
-
-		
-		
-	
-		
+		/**
+		 * Creates a Player that isAlive, with a start position, a 
+		 * current position, and health
+		 * 
+		 * @param startX  X coordinate at start
+		 * @param startY Y coordinate at start
+		 * @param startH Health amount at start
+		 */
 		public Player(int startX, int startY , int startH){
 			this.isAlive = true;
 			this.startX=startX;
@@ -26,6 +28,12 @@ public class Player {
 			
 		}
 		
+		/**
+		 * Creates a Square with a texture drawn to it and a position
+		 * @param moveX X coordinate
+		 * @param moveY Y coordinate
+		 * @param t Texture to be drawn
+		 */
 		public void addUnit(int moveX, int moveY, SqType t){
 			s = new Square(this.startX+moveX, this.startY+moveY, 64, 64, t);
 			this.posX = this.startX+moveX;
@@ -33,10 +41,17 @@ public class Player {
 			DrawSqTex(s.getTexture(),s.getX(),s.getY()-14,s.getW(),s.getH());
 		}
 		
-		public Square getS(){
-			return s;
-		}
 		
+
+		
+		/**
+		 * Checks if a Square can move to an existing Square
+		 * 
+		 * @param movement the amount of pixels it will move
+		 * @param direction the direction it will move in
+		 * @param grid the grid it will move on
+		 * @return 0 if the Square is null, 64 if not
+		 */
 		public int CheckMove(int movement, String direction, SqGrid grid){
 			Square sq = checkIfSqExists(direction,grid);
 			
@@ -48,14 +63,24 @@ public class Player {
 		}
 	
 		
+		/**
+		 * Checks if the Square in a certain direction exist
+		 * 
+		 * @param direction
+		 * @param grid
+		 * @return null if it cannot be moved too, otherwise return the position to move too
+		 */
 		public Square checkIfSqExists(String direction, SqGrid grid){
 			Square returnVal=null;
 			
 			
 			if(direction.equals("UP")){
 				returnVal = grid.getSq(getPosX()/64, (getPosY()-64)/64); 
-				if(  grid.getMapVal(getPosX()/64, (getPosY()-64)/64) == 1
-						|| grid.getMapVal(getPosX()/64, (getPosY()-64)/64)== 5
+				
+				//numbers 1,5-14 represent textures on the grid  (certain ones cannot be moved too)
+				
+				if(  grid.getMapVal(getPosX()/64, (getPosY()-64)/64) == 1 
+						|| grid.getMapVal(getPosX()/64, (getPosY()-64)/64)== 5 
 						|| grid.getMapVal(getPosX()/64, (getPosY()-64)/64)== 6
 						|| grid.getMapVal(getPosX()/64, (getPosY()-64)/64)== 7
 						|| grid.getMapVal(getPosX()/64, (getPosY()-64)/64)== 8
@@ -111,16 +136,17 @@ public class Player {
 			}
 			return returnVal;
 		}
-		
-		
-		public void setPos(int pos){
-			this.currentPos = pos;
-		}
-		
+	
+		/**
+		 * @return the X coordinate of the Players position
+		 */
 		public int getPosX(){
 			return posX;
 		}
 		
+		/**
+		 * @return the Y coordinate of the Players position
+		 */
 		public int getPosY(){
 			return posY;
 		}
