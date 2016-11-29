df0b564a145c52e3a9cfad27a46e701130d32a16
Daniel LaBorde <dlabo14@lsu.edu>
2016-11-29 03:36:00 -0600

    Update SqType.java

--- SqType.java
+++ SqType.java
@@ -2,11 +2,18 @@ package data;
 
 public enum SqType {
 	
-	Water("water"), Sand("sand"), D("dolphin"), RD("redDolphin"), MenuSDMC("menuButton")
+	Water("water"), O("outer"), D("dolphin"), RD("redDolphin"), MenuSDMC("MenuSDMC"),
+	Tree("tree"), SNW("SandNW"), SNE("SandNE"), SSW("SandSW"), SSE("SandSE"),
+	SN("SandN"), SS("SandS"), SW("SandW"), SE("SandE"), ON("outern"), OW("outerw");
+	
 	
 	String textName;
+
+
 	
 	SqType(String textName){
 		this.textName = textName;
+		
 	}
+
 }
