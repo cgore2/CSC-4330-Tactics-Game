967d296db794b81bc95962c600114500c6dc9dfd
Daniel LaBorde <dlabo14@lsu.edu>
2016-11-28 20:28:28 -0600

    Huge update to code

--- Square.java
+++ Square.java
@@ -1,43 +1,75 @@
-package dolphingame;
+package data;
 
+import static helpers.Painter.*;
 
-public class Square 
-{
-    public int row;
-    public int column;
-    
-    public Square(int r, int c)
-    {
-        row = r;
-        column = c; 
-    }
- 
-    
-    public void setRow(int r)
-    {
-        row = r;
-    }
-     
-    public void setColumn(int c)
-    {
-        column = c;
-    }
-    
-    public void setSquare(int r, int c)
-    {
-        row = r;
-        column = c; 
-    }
-    
-    
-    public int getRow()
-    {
-        return row;
-    }
-    
-    public int getColumn()
-    {
-        return column;
-    }
-         
+import org.newdawn.slick.opengl.Texture;
+
+public class Square {
+
+	private float x, y, w, h;
+	private Texture texture;
+	private SqType type;
+	
+	public Square(float x, float y, float w, float h, SqType type){
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.type = type;
+		this.texture = LoadTexture(type.textName);
+		
+	}
+	
+	public void Draw(){
+		DrawSqTex(texture, x, y, w, h);
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public float getW() {
+		return w;
+	}
+
+	public void setW(float w) {
+		this.w = w;
+	}
+
+	public float getH() {
+		return h;
+	}
+
+	public void setH(float h) {
+		this.h = h;
+	}
+
+	public Texture getTexture() {
+		return texture;
+	}
+
+	public void setTexture(Texture texture) {
+		this.texture = texture;
+	}
+
+	public SqType getType() {
+		return type;
+	}
+
+	public void setType(SqType type) {
+		this.type = type;
+	}
+	
 }
