7813a67d34fcf3816595ffbf42e9dc6785e87f4f
mmered8 <mason.mere@gmail.com>
2016-11-29 13:27:29 -0600

    Finished JavaDoc to sqaure
    most/all javadoc completed

--- Square.java
+++ Square.java
@@ -14,7 +14,7 @@ public class Square {
 	 * Creates a Square and assigns it a texture
 	 * 
 	 * @param x, the x-coordinate for the square 
-	 * @param y, the y-coordinate
+	 * @param y, the y-coordinate for the square
 	 * @param w. the width
 	 * @param h, the height
 	 * @param type, the Square type, which determines the texture of the Square
@@ -36,52 +36,90 @@ public class Square {
 		DrawSqTex(texture, x, y, w, h);
 	}
 
+        /**
+        * extracts float x value.
+        * @return float x (x coordinate)
+        */
 	public float getX() {
 		return x;
 	}
 
 	/**
-	 * @param x, x coordinate
+         * x coordinate to replace/ assign this x
+	 * @param x (float x coordinate)
 	 */
 	public void setX(float x) {
 		this.x = x;
 	}
 
+        /**
+         * extracts float y value
+         * @return y (float y coordinate)
+         */
 	public float getY() {
 		return y;
 	}
 
 	/**
-	 * @param y, y coordinate
+         * to replace/ assign this y(coordinate)
+	 * @param y, (float y coordinate) 
 	 */
 	public void setY(float y) {
 		this.y = y;
 	}
 
+        /**
+         * extracts float w value
+         * @return float w (width)
+         */
 	public float getW() {
 		return w;
 	}
 
+        /**
+         * coordinate to replace/ assign this w 
+         * @param w, (width,float)
+         */
 	public void setW(float w) {
 		this.w = w;
 	}
 
+         /**
+         * extracts float h value
+         * @return float h (height,float)
+         */
 	public float getH() {
 		return h;
 	}
 
+        /**
+         * to replace/ assign  h(height)
+         * @param h (height, float)
+         */
 	public void setH(float h) {
 		this.h = h;
 	}
 
+        /**
+         * this gets the Texture
+         * @return texture
+         */
 	public Texture getTexture() {
 		return texture;
 	}
 
+        /**
+         * this replaces/assigns the Texture texture 
+         * @param texture (texture)
+         */
 	public void setTexture(Texture texture) {
 		this.texture = texture;
 	}
 
+        /**
+         * This will set/replace the SqType 
+         * @param type SqType
+         */
 	public void setType(SqType type) {
 		this.type = type;
 	}
