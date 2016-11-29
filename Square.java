902015b62a0281b4366f3e8a2c8385034510b9fa
Daniel LaBorde <dlabo14@lsu.edu>
2016-11-29 13:11:10 -0600

    Update Square.java

--- Square.java
+++ Square.java
@@ -10,6 +10,15 @@ public class Square {
 	private Texture texture;
 	private SqType type;
 	
+	/**
+	 * Creates a Square and assigns it a texture
+	 * 
+	 * @param x, the x-coordinate for the square 
+	 * @param y, the y-coordinate
+	 * @param w. the width
+	 * @param h, the height
+	 * @param type, the Square type, which determines the texture of the Square
+	 */
 	public Square(float x, float y, float w, float h, SqType type){
 		this.x = x;
 		this.y = y;
@@ -20,6 +29,9 @@ public class Square {
 		
 	}
 	
+	/**
+	 *Draw function for drawing a Square 
+	 */
 	public void Draw(){
 		DrawSqTex(texture, x, y, w, h);
 	}
@@ -28,6 +40,9 @@ public class Square {
 		return x;
 	}
 
+	/**
+	 * @param x, x coordinate
+	 */
 	public void setX(float x) {
 		this.x = x;
 	}
@@ -36,6 +51,9 @@ public class Square {
 		return y;
 	}
 
+	/**
+	 * @param y, y coordinate
+	 */
 	public void setY(float y) {
 		this.y = y;
 	}
@@ -62,10 +80,6 @@ public class Square {
 
 	public void setTexture(Texture texture) {
 		this.texture = texture;
-	}
-
-	public SqType getType() {
-		return type;
 	}
 
 	public void setType(SqType type) {
