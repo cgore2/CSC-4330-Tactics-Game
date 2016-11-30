8693815dca032e719e668a69ef3f00f84c539df7
Daniel LaBorde <dlabo14@lsu.edu>
2016-11-30 00:22:51 -0600

    Finished Documentation

--- Start.java
+++ Start.java
@@ -18,46 +18,54 @@ public class Start {
 	public boolean entered = false;
 	Square s;
 
+	
+	/**
+	 * Moves a player in the direction desired, for the amount desired,
+	 * after checking if the move is allowed
+	 * 
+	 * @throws InterruptedException
+	 */
 	public void Move() throws InterruptedException {
 		while (Keyboard.next()) {
 			if (Keyboard.getEventKeyState()) {
+				//blue dolphin up
 				if (Keyboard.getEventKey() == Keyboard.KEY_UP) {
 					int move = this.moveYp - p.CheckMove(this.movement, "UP", grid);
 					if (moveXp != moveXe || moveYp - move != moveYe)
 						this.moveYp -= p.CheckMove(this.movement, "UP", grid);
 				}
-
+				//blue dolphin left
 				if (Keyboard.getEventKey() == Keyboard.KEY_LEFT) {
 					int move = p.CheckMove(this.movement, "LEFT", grid);
 					if (moveXp - move != moveXe || moveYp != moveYe)
 						this.moveXp -= p.CheckMove(this.movement, "LEFT", grid);
 
 				}
-
+				//blue dolphin down
 				if (Keyboard.getEventKey() == Keyboard.KEY_DOWN) {
 					int move = p.CheckMove(this.movement, "DOWN", grid);
 					if (moveXp != moveXe || moveYp + move != moveYe)
 						this.moveYp += p.CheckMove(this.movement, "DOWN", grid);
 				}
-
+				//blue dolphin right
 				if (Keyboard.getEventKey() == Keyboard.KEY_RIGHT) {
 					int move = p.CheckMove(this.movement, "RIGHT", grid);
 					if (moveXp + move != moveXe || moveYp != moveYe)
 						this.moveXp += move;
 				}
-
+				//red dolphin up using W
 				if (Keyboard.getEventKey() == Keyboard.KEY_W) {
 					int move = e.CheckMove(this.movement, "UP", grid);
 					if (moveXp != moveXe || moveYe - move != moveYp)
 						this.moveYe -= e.CheckMove(this.movement, "UP", grid);
 				}
-
+				//red dolphin left using A
 				if (Keyboard.getEventKey() == Keyboard.KEY_A) {
 					int move = e.CheckMove(this.movement, "LEFT", grid);
 					if (moveXp != moveXe - move || moveYe != moveYp)
 						this.moveXe -= e.CheckMove(this.movement, "LEFT", grid);
 				}
-
+				//red dolphin down using S
 				if (Keyboard.getEventKey() == Keyboard.KEY_S) {
 					int move = e.CheckMove(this.movement, "DOWN", grid);
 					if (moveXp != moveXe || moveYe + move != moveYp)
@@ -66,27 +74,29 @@ public class Start {
 					}
 					
 				}
-
+				//red dolphin right using D
 				if (Keyboard.getEventKey() == Keyboard.KEY_D) {
 					int move = e.CheckMove(this.movement, "RIGHT", grid);
 					if (moveXp != moveXe + move || moveYe != moveYp)
 						this.moveXe += e.CheckMove(this.movement, "RIGHT", grid);
 				}
 				
+				//PRESS F too attack with the red dolphin
 				if (Keyboard.getEventKeyState()) {
 					if (Keyboard.getEventKey() == Keyboard.KEY_F) 
 					{
-						if(p.posX - e.posX <= 256 && p.posX - e.posX >= -256 && e.isAlive)
+						if(p.posX - e.posX <= 128 && p.posX - e.posX >= -128 && e.isAlive)
 						{
 						p.health -= 1;
 						}
 					}
 				}
 				
+				//PRESS M too attack with the blue dolphin
 				if (Keyboard.getEventKeyState()) {
 					if (Keyboard.getEventKey() == Keyboard.KEY_M) 
 					{
-						if(p.posX - e.posX <= 256 && p.posX - e.posX >= -256 && p.isAlive)
+						if(p.posX - e.posX <= 128 && p.posX - e.posX >= -128 && p.isAlive)
 						{
 						e.health -= 1;
 						}
@@ -95,58 +105,32 @@ public class Start {
 			}
 			if(e.health <= 0)
 			{
-				e.isAlive = false;
+				e.isAlive = false;   //red dolphin is dead
 			}
 			if(p.health <= 0)
 			{
-				p.isAlive = false;
+				p.isAlive = false;   //blue  dolphin is dead
 			}
 			
 		}
 
 	}
 
-	public void Attack() throws InterruptedException {
-		while (Keyboard.next()) {
-			if (Keyboard.getEventKeyState()) {
-				if (Keyboard.getEventKey() == Keyboard.KEY_F) 
-				{
-					if(p.posX - e.posX <= 256 && p.posX - e.posX >= -256)
-					{
-					p.health -= 1;
-					}
-					System.out.println("TestbugF");
-				}
-			}
-			
-			if (Keyboard.getEventKeyState()) {
-				if (Keyboard.getEventKey() == Keyboard.KEY_M) 
-				{
-					System.out.println("TestbugM");
-					if(p.posX - e.posX <= 256 || p.posX - e.posX >= -256)
-					{
-					e.health -= 1;
-					}
-				}
-			}
-		}
-		if(e.health <= 0)
-		{
-			e.isAlive = false;
-		}
-		if(p.health <= 0)
-		{
-			p.isAlive = false;
-		}
-	}
 
+	/**
+	 * Starts the game
+	 * 
+	 * @throws InterruptedException
+	 */
 	public Start() throws InterruptedException {
 
-		BeginSession();
-		
+		BeginSession(); //Creates the window
 
+		
+		//Creates a map depending on which int is stored in each element of the 2D array
+		//Each number represents a certain Square with a specific texture
 		int[][] map = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 4, 14 },
-			        	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 11, 14 },  
+			        	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 11, 14 },    
 			        	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14 },
 			        	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14 }, 
 			        	{ 0, 0, 0, 0, 0, 0, 0, 5, 6, 0, 0, 0, 0, 0, 14 },
@@ -155,14 +139,20 @@ public class Start {
 			        	{ 0, 0, 0, 0, 0, 5, 6, 0, 0, 0, 0, 0, 0, 0, 14 },
 			        	{ 0, 0, 0, 0, 0, 10, 12, 0, 0, 0, 0, 0, 0, 0, 14 },
 			        	{ 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 1 } };
-
+	
+		//starts the player in square (1,0)
 		this.moveXp = 0;
 		this.moveYp = 64;
+		
+		//starts the player in square(8,13)
 		this.moveXe = 832;
 		this.moveYe = 512;
-		this.movement = 64;
+		
+		this.movement = 64; //movement amount, 64 pixels
+		
 		int hp = 5;
 		int he = 5;
+		
 		SqGrid grid = new SqGrid(map);
 		Player p1 = new Player(grid.getStartX(), grid.getStartY(), hp);
 		Player p2 = new Player(grid.getStartX(), grid.getStartY(), he);
@@ -170,9 +160,11 @@ public class Start {
 		this.p = p1;
 		this.e = p2;
 
+		//While the window is open and Q is not pressed
 		while (!Display.isCloseRequested() && Keyboard.getEventKey() != Keyboard.KEY_Q) {
-
+			
 			grid.Draw();
+			
 			if(p1.isAlive)
 			{
 			p1.addUnit(moveXp, moveYp, SqType.D);
@@ -183,19 +175,20 @@ public class Start {
 			}
 			
 			Move();
-			//Attack();
-			if (!entered) {
+
+			if (!entered){
 				DrawMenu(896,576);
 				entered = true;
 			}
-			Display.update();
-			Display.sync(60);
+			Display.update(); //updates the display
+			Display.sync(60); //60 FPS
 		}
 
-		Display.destroy();
+		Display.destroy(); //closes the window
 	}
 
 	public static void main(String[] args) throws InterruptedException {
+		
 		new Start();
 	}
 
